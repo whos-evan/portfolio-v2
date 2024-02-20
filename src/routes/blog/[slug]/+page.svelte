a9ced5890bcdba317e96191578d1996ede64684b
whos-evan <example@example.com>
2024-02-19 22:48:44 -0600

    Add meta description and author information to blog page

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -27,6 +27,10 @@
 	<title>{data.meta.title}</title>
 	<meta property="og:type" content="article" />
 	<meta property="og:title" content={data.meta.title} />
+	<meta property="og:description" content={data.meta.description} />
+	
+	<!-- author -->
+	<meta name="author" content="evan" />
 </svelte:head>
 
 <dialog id="sentitive" class="modal">
